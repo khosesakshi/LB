8d425dba49c66b9c6531b0eff23ca661edf20864
khosesakshi <sambhajikhose3@gmail.com>
2025-02-04 19:46:11 +0530

    Add files via upload

---
+++ program204.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+	int No = 87;
+	
+	printf("Number in Decimal Format : %d\n",No);
+	printf("Number in Octal Format : %o\n",No);
+	printf("Number in Hexadecimal Format : %x\n",No);
+	
+	return 0;
+}
