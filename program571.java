3463882d9c295e68e12b56d621c7607cf466250e
khosesakshi <sambhajikhose3@gmail.com>
2025-02-04 22:17:27 +0530

    Add files via upload

---
+++ program571.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class program571
+{
+    public static void main(String Arg[])
+    {
+       int Arr[][] = new int[4][3];
+
+       System.out.println(Arr.length);
+       System.out.println(Arr[0].length);
+    }
+}
